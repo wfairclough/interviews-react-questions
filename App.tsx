d97f803347107040f049617707854f047782fb67
wfairclough <wfairclough@gmail.com>
2022-07-26 10:43:05 -0400

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import './style.css';
+
+interface Property {
+  name: string;
+  type: string;
+  city: string;
+}
+
+const properties: Property[] = [
+  {
+    name: 'SuiteSpot 1',
+    type: 'residential',
+    city: 'Toronto',
+  },
+  {
+    name: 'SuiteSpot 2',
+    type: 'residential',
+    city: 'Vancouver',
+  },
+  {
+    name: 'commercial_suitespot',
+    type: 'commercial',
+    city: 'Calgary',
+  },
+];
+
+export default function App() {
+  return (
+    <main>
+      <h1>Questions</h1>
+      <ul>
+        <li>Find all the residental properties?</li>
+        <li>
+          Find all properties with 'SuiteSpot' in the name (case insensitive)?
+        </li>
+      </ul>
+
+      <h2>Properties</h2>
+      <pre>{JSON.stringify(properties, null, 2)}</pre>
+    </main>
+  );
+}
